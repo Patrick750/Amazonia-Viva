b26ab9f95c5890eab016cae36d0b7732696ae210
Patrick Ortiz <ortizpatrick750@gmail.com>
2026-02-18 15:09:16 -0500

    avances

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'core/login.html')
+
+def signup(request):
+    return render(request, 'core/signup.html')
