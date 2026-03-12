c8fff3de395b19df57ac397263a1cc03b1c9622c
Patrick <ortizpatrick750@gmail.com>
2026-03-12 00:57:03 -0500

    logo actualizado

--- frontend_project/src/components/header.vue
+++ frontend_project/src/components/header.vue
@@ -40,9 +40,7 @@ const cerrarSesion = async () => {
   <nav class="bg-white border-b border-gray-100 px-6 py-4 flex items-center justify-between w-full sticky top-0 z-50 shadow-sm">
     
     <router-link to="/" class="flex items-center gap-3 hover:opacity-80 transition-opacity">
-      <div class="bg-teal-500 text-white w-10 h-10 flex items-center justify-center rounded-xl font-bold text-xl shadow-sm">
-        T
-      </div>
+      <img src="../assets/public/amazon_logo.png" class="w-12" alt="">
       <span class="text-xl font-bold text-gray-950 tracking-tight">Amazonia viva</span>
     </router-link>
 
