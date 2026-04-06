55dde22087879775d9ebe1e77675b8c554ec7106
Patrick <ortizpatrick750@gmail.com>
2026-04-06 09:23:39 -0500

    Configuracion para despliegle en settings

--- amazoniaviva/settings.py
+++ amazoniaviva/settings.py
@@ -153,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Modelos propios
 # Le dice a Django: "Olvida tu usuario por defecto y usa el mío"
